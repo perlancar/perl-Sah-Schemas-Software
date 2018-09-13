ec676d2126c8b28d9eb68f820e7a91a381e71ee8
perlancar (on pc-office) <perlancar@gmail.com>
2018-09-13 21:18:24 +0700

    Initial commit

---
+++ lib/Sah/Schema/software/username.pm
@@ -0,0 +1,22 @@
+package Sah::Schema::software::arch;
+
+# DATE
+# VERSION
+
+# XXX currently very simple, will be improved upon later
+
+our $schema = ["str", {
+    summary => 'Software architecture name',
+    in => [qw/
+                 linux-x86
+                 linux-x86_64
+                 win32
+                 win64
+             /];
+}, {}];
+
+1;
+
+# ABSTRACT:
+
+=head1 SEE ALSO
---
+++ lib/Sah/Schemas/Software.pm
@@ -0,0 +1,7 @@
+package Sah::Schemas::Software;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Schemas related to software
